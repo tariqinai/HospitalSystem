7a88d09de9b7ce29b68cab597a1e6cd5abbd87fb
Hafiz Muhammad Tariq Raza <muhammadtariq2001@outlook.com>
2020-12-19 20:27:14 +0500

    working on back end

---
+++ Database&Normalization/Insertion/Staff/insert.php
@@ -0,0 +1,17 @@
+<?php
+  $sid = $_POST['sid'];
+  $sname = $_POST['sname'];
+  $scontact = $_POST['scontact'];
+  $scnic = $_POST['scnic'];
+  $saddress = $_POST['saddress'];
+  $ssalary = $_POST['ssalary'];
+  $sdesignation = $_POST['sdesignation'];
+  $sshift = $_POST['sid'];
+  
+  /* Logic of DB */
+  $conn = mysqli_connect('localhost', 'root', '', 'hospital');
+
+
+
+
+?>
