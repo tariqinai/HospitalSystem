dea9578609514df684076fc9d5a4dee3a353d478
Hafiz Muhammad Tariq Raza <muhammadtariq2001@outlook.com>
2020-12-20 00:15:04 +0500

    Backend For staff is nearly end. with submit button add to new page

--- Database&Normalization/Insertion/Staff/insert.php
+++ Database&Normalization/Insertion/Staff/insert.php
@@ -1,17 +1,29 @@
 <?php
-  $sid = $_POST['sid'];
-  $sname = $_POST['sname'];
-  $scontact = $_POST['scontact'];
-  $scnic = $_POST['scnic'];
-  $saddress = $_POST['saddress'];
-  $ssalary = $_POST['ssalary'];
-  $sdesignation = $_POST['sdesignation'];
-  $sshift = $_POST['sid'];
-  
-  /* Logic of DB */
-  $conn = mysqli_connect('localhost', 'root', '', 'hospital');
+$sid = $_POST['sid'];
+$sname = $_POST['sname'];
+$scontact = $_POST['scontact'];
+$scnic = $_POST['scnic'];
+$saddress = $_POST['saddress'];
+$ssalary = $_POST['ssalary'];
+$sdesignation = $_POST['sdesignation'];
+$sshift = $_POST['sid'];
 
-
-
-
-?>
+/* Logic of DB */
+$conn = mysqli_connect('localhost', 'root', '', 'hospital');
+if (!$conn) {
+  die("Connection Failed. " . mysqli_connect_error());
+}
+$query = "INSERT INTO `staff` (`s_id`, `s_Name`, `s_Contact`, `s_CNIC`, `s_Address`, `s_Salary`, `s_Desigation`, `s_Shift`) 
+  VALUES ('$sid', '$sname', '$scontact', '$scnic', '$saddress', '$ssalary', '$sdesignation', '$sshift')";
+$myResult = mysqli_query($conn, $query);
+if (mysqli_num_rows($myResult) > 0) {
+  echo '<h1>' . 'Record Already Exist' . '</h1>';
+} 
+else {
+  if (mysqli_query($conn, $query)) {
+    echo "Record Insertion Succeed";
+  } else {
+    echo "Error" . $query . "<br>" . mysqli_error($conn);
+  }
+}
+mysqli_close($conn);
