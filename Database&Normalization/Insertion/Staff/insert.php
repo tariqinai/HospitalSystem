1d439eef05f48467d37cda14f63044a822b63186
Hafiz Muhammad Tariq Raza <muhammadtariq2001@outlook.com>
2020-12-22 22:21:37 +0500

    Completestaff with AJAX with error handle too Now move to deletion and updataion and some other tricky steps

--- Database&Normalization/Insertion/Staff/insert.php
+++ Database&Normalization/Insertion/Staff/insert.php
@@ -1,34 +1,40 @@
 <?php
-  $sid = $_POST['s_Id'];
-  $sname = $_POST['s_Name'];
-  $scontact = $_POST['s_Contact'];
-  $scnic = $_POST['s_CNIC'];
-  $saddress = $_POST['s_Address'];
-  $ssalary = $_POST['s_Salary'];
-  $sdesignation = $_POST['s_Designation'];
-  $sshift = $_POST['s_Shift'];
+  $sid = $_GET['s_Id'];
+  $sname = $_GET['s_Name'];
+  $scontact = $_GET['s_Contact'];
+  $scnic = $_GET['s_CNIC'];
+  $saddress = $_GET['s_Address'];
+  $ssalary = $_GET['s_Salary'];
+  $sdesignation = $_GET['s_Designation'];
+  $sshift = $_GET['s_Shift'];
 
   /* Logic of DB */
-  $conn = mysqli_connect('localhost', 'root', '', 'carecenter');
-  if (!$conn) {
-    die("Connection Failed. ");
-  } 
+  if(empty($sid) And empty($sname) And empty($scontact) And empty($scnic) And empty($saddress) And empty($ssalary) And empty($sdesignation) And empty($sshift) ){
+    echo '<q>'."Please Enter Some Record ...".'</q>';
+  }
   else{
-    $queryCheck = " SELECT * FROM staff WHERE s_id = $sid ";
-    $myResultCheck = mysqli_query($conn, $queryCheck);
-    if ( mysqli_num_rows($myResultCheck) > 0 ) { 
-      echo '<h1>' . 'Record Already Exist' . '</h1>'; 
-    }
+  $conn = mysqli_connect('localhost', 'root', '', 'carecenter');
+    if (!$conn) {
+      die("Connection Failed. ");
+    } 
     else{
-      $query = "INSERT INTO `staff` (`s_id`, `s_Name`, `s_Contact`, `s_CNIC`, `s_Address`, `s_Salary`, `s_Designation`, `s_Shift`) 
-      VALUES ('$sid', '$sname', '$scontact', '$scnic', '$saddress', '$ssalary', '$sdesignation', '$sshift')";  
-      if (mysqli_query($conn, $query)) {
-        echo '<h1>'."Record Insertion Succeed".'</h1>';
-      } 
-      else {
-        echo "Error" . $query . "<br>" . mysqli_error($conn);  
+      $queryCheck = " SELECT * FROM staff WHERE s_id = $sid ";
+      $myResultCheck = mysqli_query($conn, $queryCheck);
+      if ( mysqli_num_rows($myResultCheck) > 0 ) { 
+        echo '<q>'.'Record Already Exist'.'</q>'; 
+      }
+      else{
+        $query = "INSERT INTO `staff` (`s_id`, `s_Name`, `s_Contact`, `s_CNIC`, `s_Address`, `s_Salary`, `s_Designation`, `s_Shift`) 
+        VALUES ('$sid', '$sname', '$scontact', '$scnic', '$saddress', '$ssalary', '$sdesignation', '$sshift')";  
+        if (mysqli_query($conn, $query)) {
+          echo '<q>'."Record Insertion Succeed".'</q>';
+        } 
+        else {
+          echo "Error" . $query . "<br>" . mysqli_error($conn);
+        }
       }
     }
+    mysqli_close($conn);
   }
-  mysqli_close($conn);
+  
 ?>
