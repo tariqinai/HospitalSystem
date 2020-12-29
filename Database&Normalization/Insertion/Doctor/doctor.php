3528d118cf020a367ee263768622c735d601cd26
Hafiz Muhammad Tariq Raza <muhammadtariq2001@outlook.com>
2020-12-29 16:32:08 +0500

    Insert in Doctor

--- Database&Normalization/Insertion/Doctor/doctor.php
+++ Database&Normalization/Insertion/Doctor/doctor.php
@@ -1,49 +1,28 @@
 <?php
-    $sid = $_GET['s_Id'];
-    $sname = $_GET['s_Name'];
-    $scontact = $_GET['s_Contact'];
-    $scnic = $_GET['s_CNIC'];
-    $saddress = $_GET['s_Address'];
-    $ssalary = $_GET['s_Salary'];
-    $sdesignation = $_GET['s_Designation'];
-    $sshift = $_GET['s_Shift'];
+    $did = $_GET['D_id'];
+    $dspecialization = $_GET['D_Specialization'];
+    $sid = $_GET['s_id'];
+    
 
 /* Logic of DB */
-if (empty($sname)) {
-  $sname = NULL;
+if (empty($dspecialization)) {
+  $dspecialization = NULL;
 }
-if (empty($scontact)) {
-  $scontact = NULL;
-}
-if (empty($scnic)) {
-  $scnic = NULL;
-}
-if (empty($saddress)) {
-  $saddress = NULL;
-}
-if (empty($ssalary)) {
-  $ssalary = NULL;
-}
-if (empty($sdesignation)) {
-  $sdesignation = NULL;
-}
-if (empty($sshift)) {
-  $sshift = NULL;
-}
-if (empty($sid)) {
-  echo "Warning! Must enter Primary Key.";
+if (empty($did) or empty($sid) ) {
+  echo "Warning! Must enter Primary Key(did) And Foreign Key(sid).";
 } else {
 
   $conn = mysqli_connect('localhost', 'root', '', 'carecenter');
   if (!$conn) {
     die("Connection Failed. ");
   } else {
-    $queryCheck = " SELECT * FROM staff WHERE s_id = $sid ";
+    $queryCheck = "SELECT * FROM `doctor` WHERE D_id = $did ";
     $myResultCheck = mysqli_query($conn, $queryCheck);
     if (mysqli_num_rows($myResultCheck) > 0) {
       echo '<q>' . 'Record Already Exist' . '</q>';
-    } else {
-      $query = "INSERT INTO `staff` (`s_id`, `s_Name`, `s_Contact`, `s_CNIC`, `s_Address`, `s_Salary`, `s_Designation`, `s_Shift`) VALUES ('$sid', '$sname', '$scontact', '$scnic', '$saddress', '$ssalary', '$sdesignation', '$sshift')";
+    } 
+    else {
+      $query = "INSERT INTO `doctor` (`D_id`, `D_Specialization`, `s_id`) VALUES ('$did', '$dspecialization', '$sid')";
       if (mysqli_query($conn, $query)) {
         echo '<q>' . "Record Insertion Succeed" . '</q>';
       } else {
